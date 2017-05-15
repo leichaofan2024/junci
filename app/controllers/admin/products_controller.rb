fc7962daab1203103cb8ba95c80df0abeb6914ed
leichaofan <leichaofan2024@gmail.com>
2017-05-15 16:42:02 +0800

    add require_is_admin and build seed

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  
+  before_action :require_is_admin
   before_action :find_product, only: [:edit, :update, :destroy ]
   def index
     @products= Product.all
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.first
+u.is_admin = true
+u.save
+puts "创建完成"
