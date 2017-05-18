6f1448b33dd2549d39b62f634a10d612b17ed910
leichaofan <leichaofan2024@gmail.com>
2017-05-18 17:01:03 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  validates :billing_name, presence: true
+  validates :billing_address, presence: true
+  validates :shipping_name, presence: true
+  validates_presence_of :shipping_address
+
+  belongs_to :user 
+
 end
