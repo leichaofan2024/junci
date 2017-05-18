a9e0b9ec011115fd4a8d0449c16477d0905f2133
leichaofan <leichaofan2024@gmail.com>
2017-05-19 01:56:48 +0800

    users can see their orders

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
+  namespace :account do
+    resources :orders
+  end
+
   namespace :admin do
     resources :products
   end
@@ -17,5 +21,5 @@ Rails.application.routes.draw do
     end
   end
   resources :cart_items
-  resources :orders 
+  resources :orders
 end
