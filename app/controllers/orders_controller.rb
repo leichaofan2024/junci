2ac51ba26e1285643ac0b2bf6e56cf1d4a3037ed
leichaofan <leichaofan2024@gmail.com>
2017-05-18 18:58:44 +0800

    fix some bug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,15 +9,15 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     @order.user = current_user
-    @order.total = current_cart.tatal_price
+    @order.total = current_cart.total_price
     if @order.save
       current_cart.cart_items.each do |cart_item|
         product_list = ProductList.new
-        producdt.order = @order 
+        product_list.order = @order
         product_list.product_name = cart_item.product.title
         product_list.product_price = cart_item.product.price
         product_list.quantity = cart_item.quantity
-        proudct_list.save
+        product_list.save
       end
       current_cart.cart_items.destroy_all
       redirect_to order_path(@order)
