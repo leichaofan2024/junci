f67e85067ac5f5341d9013bec49bf38c9f01aa29
leichaofan <leichaofan2024@gmail.com>
2017-05-21 13:20:45 +0800

    fix bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u = User.first
-u.is_admin = true
-u.save
-puts "创建完成"
+User.create(email: "yaojunci@admin.com", password: "123456",password_confirmation:"123456", is_admin: true )
